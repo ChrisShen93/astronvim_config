d233a4be37e66db03adb0d747a5709526ebc578c
ChrisShen93 <chrisshen93@gmail.com>
2025-01-16 17:45:24 +0800

    not show file_size file_type last_modified created columns in neo-tree

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -114,4 +114,24 @@ return {
       }
     end,
   },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        file_size = {
+          enabled = false,
+        },
+        type = {
+          enabled = false,
+        },
+        last_modified = {
+          enabled = false,
+        },
+        created = {
+          enabled = false,
+        },
+      },
+    },
+  },
 }
